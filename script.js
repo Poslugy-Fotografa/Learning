f308ffdb3ddbb456006cdb696080afc7d671ea4a
Serge <sr1977ka@gmail.com>
2021-05-14 19:44:57 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Коммент
+// alert('Це - гарний суперсайт! Хто не згодний - іде в дупу!');
+
+// let a = prompt('Добрий день! Як вас звуть?');
+
+// document.querySelector('h2').innerHTML = 'Привіт, ' + a + '!';
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+// console.log(typeof (answer));aa
+
+// Number
+// String
+// Boolean
+// Object
+// null
+// undefined
+
+
+let result = 'arr'
+
+console.log(result);
